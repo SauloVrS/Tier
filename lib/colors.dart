ecf061267bd1e9db2571ef055c458eb49a7b6879
Leticia Mota <le.mota00@gmail.com>
2022-05-03 22:00:07 -0300

    adicionei um arquivo de cores e o googlefonts

---
+++ lib/colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class AppColor{
+  static Color background = const Color(0xFFfbfcff);
+  static Color textosPretos1 = const Color(0x991B1B1B);
+  static Color textosPretos2 = const Color(0xCD1B1B1B);
+  static Color textosPretos3 = const Color(0xCD000000);
+  static Color textoBranco = const Color(0xFFffffff);
+  static Color amareloEscuro = const Color(0xFFffb761);
+  static Color amareloPrincipal = const Color(0xE6FFC368);
+  static Color amareloPastel = const Color(0xFFFFE9C2);
+  static Color cinzaBranco = const Color(0xffe5e5e5);
+  static Color cinzaClaro = const Color(0xFFc4c4c4);
+  static Color cinzaMedio = const Color(0xFFaaaaaa);
+  static Color cinzaclaroAppBar = const Color(0x4D000000);
+  static Color cinzaescuroAppBar = const Color(0xCC000000);
+  static Color cobre = const Color(0x80C56914);
+  static Color prata = const Color(0x80CDD0E3);
+}
