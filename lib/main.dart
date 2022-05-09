0f308bc6b963aa810b0d659fa25683503bceb9f2
caylane <paivacaylane@gmail.com>
2022-05-09 17:00:09 -0300

    debugShowCheckedMode

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tier',
       theme: ThemeData(
         primarySwatch: Colors.blue,
