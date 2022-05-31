40616009be1f76556eb988e6393428bddf6f37b9
GabrielSales10 <gabrielsales390@gmail.com>
2022-05-31 15:43:17 -0300

    Funcionamento da tela de busca

--- lib/views/lista_lojas.dart
+++ lib/views/lista_lojas.dart
@@ -1,3 +1,4 @@
+import 'package:tier/widgets/buscar_functions/busca_functions.dart';
 import 'package:tier/widgets/loja_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,7 +7,8 @@ import '../widgets/bottom_nav_bar.dart';
 
 class ListaLojas extends StatefulWidget {
   //final String pesquisa;
-  const ListaLojas({Key? key}) : super(key: key);
+  final String text;
+  const ListaLojas({Key? key, required this.text}) : super(key: key);
 
   @override
   State<ListaLojas> createState() => _ListaLojasState();
@@ -22,55 +24,56 @@ class _ListaLojasState extends State<ListaLojas> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           //Filtro(),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Text('Resultados Semelhantes a ração',style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18.0),),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Text('Resultados Semelhantes a ${widget.text}',style: TextStyle(fontWeight: FontWeight.w800, fontSize: 18.0),),
           ),
-          ListView(
-            shrinkWrap: true,
-            children: const [
-              LojaWidget(
-                imagem: 'images/loja_racao1.jpg',
-                distancia: 1.1,
-                isOpen: true,
-                nomeLoja: 'Top Rações',
-                nota: 4.3,
-                precoEntrega: 7.89,
-                tempoMaximo: 70,
-                tempoMinimo: 50,
-              ),
-              LojaWidget(
-                imagem: 'images/imagem_loja_racao.jpg',
-                distancia: 2.4,
-                isOpen: false,
-                nomeLoja: 'Rações HercoSul',
-                nota: 3.7,
-                precoEntrega: 4.63,
-                tempoMaximo: 80,
-                tempoMinimo: 40,
-              ),
-              LojaWidget(
-                imagem: 'images/loja_racao_novo.jpg',
-                distancia: 5.7,
-                isOpen: true,
-                nomeLoja: 'Atacadão da Ração',
-                nota: 2.8,
-                precoEntrega: 12.53,
-                tempoMaximo: 90,
-                tempoMinimo: 60,
-              ),
-              LojaWidget(
-                imagem: 'images/loja_racao4.jpg',
-                distancia: 4.2,
-                isOpen: false,
-                nomeLoja: 'Ração em Casa',
-                nota: 5.0,
-                precoEntrega: 12.67,
-                tempoMaximo: 40,
-                tempoMinimo: 20,
-              ),
-            ],
-          )
+          streamLojasBusca(widget.text),
+          // ListView(
+          //   shrinkWrap: true,
+          //   children: const [
+          //     LojaWidget(
+          //       imagem: 'images/loja_racao1.jpg',
+          //       distancia: 1.1,
+          //       isOpen: true,
+          //       nomeLoja: 'Top Rações',
+          //       nota: 4.3,
+          //       precoEntrega: 7.89,
+          //       tempoMaximo: 70,
+          //       tempoMinimo: 50,
+          //     ),
+          //     LojaWidget(
+          //       imagem: 'images/imagem_loja_racao.jpg',
+          //       distancia: 2.4,
+          //       isOpen: false,
+          //       nomeLoja: 'Rações HercoSul',
+          //       nota: 3.7,
+          //       precoEntrega: 4.63,
+          //       tempoMaximo: 80,
+          //       tempoMinimo: 40,
+          //     ),
+          //     LojaWidget(
+          //       imagem: 'images/loja_racao_novo.jpg',
+          //       distancia: 5.7,
+          //       isOpen: true,
+          //       nomeLoja: 'Atacadão da Ração',
+          //       nota: 2.8,
+          //       precoEntrega: 12.53,
+          //       tempoMaximo: 90,
+          //       tempoMinimo: 60,
+          //     ),
+          //     LojaWidget(
+          //       imagem: 'images/loja_racao4.jpg',
+          //       distancia: 4.2,
+          //       isOpen: false,
+          //       nomeLoja: 'Ração em Casa',
+          //       nota: 5.0,
+          //       precoEntrega: 12.67,
+          //       tempoMaximo: 40,
+          //       tempoMinimo: 20,
+          //     ),
+          //   ],
+          // )
         ],
       ),
     );
---
+++ lib/widgets/buscar_functions/busca_functions.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:tier/views/loja_page.dart';
+
+import '../../firebase/loja_helper.dart';
+import '../../firebase/produto_helper.dart';
+import '../lojas_list.dart';
+import '../promocoes_list.dart';
+
+Widget streamProdutosBusca({required String id, required String text, required BuildContext context}) {
+
+  return StreamBuilder<List<Produto>>(
+      stream: getProdutosBusca(id: id),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong! ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          final produtos = snapshot.data!;
+          List<Produto> newProdutos = produtos.where((element) => element.nome.toLowerCase().contains(text.toLowerCase())).toList();
+          // produtos.forEach((element) {
+          //   if(element.nome == text){
+          //     newProdutos.add(element);
+          //   }
+          // });
+          return SizedBox(
+            //height: 240,
+            child: ListView(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              //scrollDirection: Axis.horizontal,
+              children: newProdutos.map((produto) => prodList(produto, id, context)).toList(),
+            ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      }
+  );
+}
+
+Widget streamIdsBusca(String text) {
+
+  return StreamBuilder<List<String>>(
+      stream: getIds(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong! ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          final ids = snapshot.data!;
+          return SizedBox(
+              //height: 250,
+              child: ListView(
+                padding: const EdgeInsets.only(left: 15),
+                shrinkWrap: true,
+                //scrollDirection: Axis.horizontal,
+                physics: const NeverScrollableScrollPhysics(),
+                children: ids.map((id) => streamProdutosBusca(id: id, text: text, context: context)).toList(),
+              )
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      }
+  );
+
+}
+
+Stream<List<Produto>> getProdutosBusca({required String id}) =>
+    FirebaseFirestore.instance
+        .collection("lojas")
+        .doc(id)
+        .collection('produtos')
+        .snapshots()
+        .map((snapshot) =>
+        snapshot.docs.map((doc) => Produto.fromJson(doc.data())).toList());
+
+// parte da loja
+
+Widget streamLojasBusca(String text){
+  return StreamBuilder<List<Loja>>(
+      stream: readUsers(),
+      builder: (context, snapshot){
+        if(snapshot.hasError){
+          return Text('Something went wrong! ${snapshot.error}');
+        } else if(snapshot.hasData){
+          final lojas = snapshot.data!;
+          List<Loja> newLojas = lojas.where((element) => element.nome.toLowerCase().contains(text.toLowerCase())).toList();
+          return ListView(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: newLojas.map((loja) => lojaList(loja, 30, context)).toList(),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      }
+  );
+}
