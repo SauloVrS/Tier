55916532cd472b0cd2d9a31a45a3064df9b44dab
Leticia Mota <le.mota00@gmail.com>
2022-05-31 17:33:15 -0300

    raynara

--- lib/views/configuracoes_pages/politicas_de_uso.dart
+++ lib/views/configuracoes_pages/politicas_de_uso.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tier/views/perfil_pages/perfil_usuario.dart';
 import 'package:tier/colors.dart';
 import 'package:tier/widgets/bottom_nav_bar.dart';
 
@@ -10,7 +11,64 @@ class Politicas_uso extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            Text('Políticas de Uso',
+              style: GoogleFonts.poppins(
+                color: Color.fromRGBO(27, 27, 27, 0.8),
+                fontSize: 22,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: (){ Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context) => TelaPerfilUsuario(),
+            ));
+            },
+            icon: const Icon( Icons.arrow_back_ios, color: Color(0xFF6E6E6E))
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 30,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan( text: 'Termos de uso ', style: GoogleFonts.poppins( fontSize: 23, color: Colors.orangeAccent),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan( text: 'Por meio do presente instrumento, são estabelecidos os termos e as condições gerais para a prestação de serviços (as “Condições Gerais”), os quais ficam incorporados, para todos os fins e efeitos, ao formulário de contratação do iFood (o “Formulário” e, emconjunto com a Condições Gerais, o “Contrato”). ',
+                  style: GoogleFonts.poppins( fontSize: 16, color: Colors.black54),
+                ),
+              ),
+              const SizedBox(height: 30,),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan( text: 'Definições ', style: GoogleFonts.poppins( fontSize: 23, color: Colors.orangeAccent),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan( text: '1.1.Os termos e as expressões abaixo, quando iniciados em letra maiúscula nestes Termos e Condições ou no Contrato, no singular ou no plural, no masculino ou no feminino, terão os significados que lhes são indicados ao longo do Contrato ou abaixo: “Colaboradores” significa os funcionários e/ou colaboradores da Empresa que poderão utilizar o Benefício.“Contas” significa as contas individuais e pessoais de cada um dos Colaboradores na Plataforma iFood.“Pedidos” significa os pedidos de delivery do gênero de comidas e bebidas comercializados pelo Restaurante.“Plataforma iFood” significa todos e quaisquer websites e aplicativos para celulares de titularidade do iFood, por meio dos quais, dentre outras funcionalidades, (a) o Restaurante, de um lado, pode ofertar os produtos do seu cardápio aos Clientes Finais, e (b) os Clientes Finais, de outro lado, podem fazer pedidos de entrega de tais produtos aos Restaurantes.“Pedidos” significam os pedidos de entrega de gêneros alimentícios e bebidas realizados pelos clientes finais aos Restaurantes por meio da Plataforma iFood.“Remuneração” significa o valor devido por mês, em decorrência da prestação dos Serviços e do licenciamento do Software.“Restaurantes” significa restaurantes, bares, padarias e outros estabelecimentos similares.“Serviços” significa os serviços de controle de despesas realizadas pelos Colaboradores por meio da Plataforma iFood, a coleta e interpretação dos dados referentes às referidas despesas e aos Pedidos por eles realizados.“Software” significa o software chamado “iFood para Empresas”.',
+                  style: GoogleFonts.poppins( fontSize: 16, color: Colors.black54),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/views/perfil_pages/meu_codigo_perfil.dart
+++ lib/views/perfil_pages/meu_codigo_perfil.dart
@@ -6,194 +6,165 @@ import 'package:tier/widgets/bottom_nav_bar.dart';
 import 'package:tier/views/adocao-home.dart';
 import 'package:tier/views/perfil_pages/perfil_usuario.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:share_plus/share_plus.dart';
 
-class Meu_cupom extends StatefulWidget {
+class Meu_cupom extends StatelessWidget {
   const Meu_cupom({Key? key}) : super(key: key);
 
-  @override
-  State<Meu_cupom> createState() => _Meu_cupomState();
-}
 
-class _Meu_cupomState extends State<Meu_cupom> {
+  void share(){
+    Share.share("Share text");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Column(
-            children: [
-              Text('MEU CÓDIGO',
-                  style: GoogleFonts.poppins(
-                  color: Color.fromRGBO(27, 27, 27, 0.8),
-                  fontSize: 22,
-                  fontWeight: FontWeight.w400,
-                ),
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            Text('MEU CÓDIGO',
+              style: GoogleFonts.poppins(
+                color: Color.fromRGBO(27, 27, 27, 0.8),
+                fontSize: 22,
+                fontWeight: FontWeight.w400,
               ),
-            ],
-          ),
-          centerTitle: true,
-          leading: IconButton(
-              onPressed: (){ Navigator.pushReplacement(context, MaterialPageRoute(
-                builder: (context) => TelaPerfilUsuario(),
-              ));
-              },
-              icon: const Icon( Icons.arrow_back_ios, color: Color(0xFF6E6E6E))
-          )
+            ),
+          ],
+        ),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: (){ Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context) => TelaPerfilUsuario(),
+            ));
+            },
+            icon: const Icon( Icons.arrow_back_ios, color: Color(0xFF6E6E6E))
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-        Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 250,
-                width:  300,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(
-                            "images/meu_codigo.png",
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width:  300,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              "images/meu_codigo.png",
+                            )
                         )
-                    )
-                ),
-              ),
-              const SizedBox(height: 40,),
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan( text: 'Indique o ', style: GoogleFonts.poppins( fontSize: 30, color: Colors.black),
-                    children: <TextSpan> [
-                      TextSpan( text: 'Tier', style: TextStyle(color: Colors.orangeAccent)),
-                      TextSpan( text: ' para os ', style: TextStyle(color: Colors.black)),
-                    ]
-                ),
-              ),
-              RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan( text: ' amigos e ganhe', style: GoogleFonts.poppins( fontSize: 30, color: Colors.black)),
-              ),
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan( text: ' 100 pontos', style: GoogleFonts.poppins( fontSize: 30, color: Colors.orangeAccent)),
-              ),
-              const SizedBox(height: 20,),
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                    text: 'Compartilhe seu código com os \n',
-                    style: GoogleFonts.poppins(
-                        fontSize: 13,
-                        color: AppColor.textosPretos3),
-                    children: <TextSpan> [
-                      TextSpan( text: 'amigos que ainda não tem Tier. \n'),
-                      TextSpan( text: 'Ambos ganham 100 pontos!')
-                    ]
-                ),
-              ),
-              const SizedBox(height: 20,),
-              Center(
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    height: 80,
-                    width: (MediaQuery.of(context).size.width) - 100,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 10,
-                            color: AppColor.cinzaBranco,
-                          ),
+                    ),
+                  ),
+                  const SizedBox(height: 40,),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan( text: 'Indique o ', style: GoogleFonts.poppins( fontSize: 30, color: Colors.black),
+                        children: <TextSpan> [
+                          TextSpan( text: 'Tier', style: TextStyle(color: Colors.orangeAccent)),
+                          TextSpan( text: ' para os ', style: TextStyle(color: Colors.black)),
                         ]
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  ),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan( text: ' amigos e ganhe', style: GoogleFonts.poppins( fontSize: 30, color: Colors.black)),
+                  ),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan( text: ' 100 pontos', style: GoogleFonts.poppins( fontSize: 30, color: Colors.orangeAccent)),
+                  ),
+                  const SizedBox(height: 20,),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                        text: 'Compartilhe seu código com os \n',
+                        style: GoogleFonts.poppins(
+                            fontSize: 13,
+                            color: AppColor.textosPretos3),
+                        children: <TextSpan> [
+                          TextSpan( text: 'amigos que ainda não tem Tier. \n'),
+                          TextSpan( text: 'Ambos ganham 100 pontos!')
+                        ]
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  Center(
+                      child: Container(
+                        margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        height: 80,
+                        width: (MediaQuery.of(context).size.width) - 100,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 10,
+                                color: AppColor.cinzaBranco,
+                              ),
+                            ]
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-
-                            Column(
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
 
-                                RichText(
-                                  textAlign: TextAlign.left,
-                                  text: TextSpan(
-                                    text: ' TX303V3BTT',
-                                    style: GoogleFonts.poppins(
-                                        fontSize: 20,
-                                        color: AppColor.textosPretos3),
-                                  ),
-                                ),
+                                Column(
+                                  children: [
 
-                                RichText(
-                                  textAlign: TextAlign.center,
-                                  text: TextSpan(
-                                    text: 'copiar código',
-                                    style: GoogleFonts.poppins(
-                                        fontSize: 13,
-                                        color: AppColor.textosPretos1),
+                                    RichText(
+                                      textAlign: TextAlign.left,
+                                      text: TextSpan(
+                                        text: ' TX303V3BTT',
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 20,
+                                            color: AppColor.textosPretos3),
+                                      ),
+                                    ),
 
-                                  ),
+                                    RichText(
+                                      textAlign: TextAlign.center,
+                                      text: TextSpan(
+                                        text: 'copiar código',
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 13,
+                                            color: AppColor.textosPretos1),
+
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
-                            ),
+                            )
                           ],
-                        )
+                        ),
+                      )
+                  ),
+                  const SizedBox(height: 30,),
+                  Center(
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: (){
+                            share();
+                          },
+                          child: Text("Share Text"),
+                        ),
                       ],
                     ),
                   )
-              ),
-              const SizedBox(height: 30,),
 
-              GestureDetector(
-                onTap: (){
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(
-                        builder: (context) => MainLojas(),//colocar o share option aqui
-                      )
-                  );
-                },
-                child: Container(
-                  width: 180,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: AppColor.amareloPrincipal.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColor.cinzaBranco,
-                          blurRadius: 0.8,
-                          offset: const Offset(2, 2),
-                        ),
-                      ]
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Compartilhe',
-                            style: GoogleFonts.poppins(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500,
-                              color: AppColor.textoBranco,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              )
+                ],
+              ),
             ],
           ),
-        ],
-      ),
         ],
       ),
     );
