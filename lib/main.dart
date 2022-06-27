7bcd1bcc1af46b791295c05cd9f3bdb72b8bc738
Raynara Coelho <60441829+raynaraglit23@users.noreply.github.com>
2022-06-27 10:15:25 -0300

    início da internacionalização

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:tier/firebase/firebase_tier.dart';
 import 'package:tier/widgets/auth_widgets/login_page.dart';
 import 'package:tier/views/main_lojas.dart';
+import 'package:intl/intl.dart';
 
 import 'views/splash_screen.dart';
 
@@ -20,6 +21,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      supportedLocales: [
+        Locale('en', ''), // English, no country code
+        Locale('es', ''), // Spanish, no country code
+        Locale('pt', ''),
+      ],
       title: 'Tier',
       theme: ThemeData(
         primarySwatch: Colors.orange,
